ca64f5a268856496502260b5969e194d41508e9c
RicardoFazzi <ricardofazzi007@gmail.com>
2016-02-24 21:55:49 -0300

    WIP: Avance con pruebas de GRUNT. FALLA a la hora de hacer cssmin y uglify, hay que revisar por que. una vez que eso ande, con grunt serve levantamos el servidor local con livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,9 @@
 module.exports = function(grunt){
 
     require('time-grunt')(grunt);
-    require('jit-grunt')(grunt);
+    require('jit-grunt')(grunt, {
+        useminPrepare: 'grunt-usemin'
+    });
 
     //Define the configuration for all the tasks
     grunt.initConfig({
@@ -20,35 +22,119 @@ module.exports = function(grunt){
                     'Gruntfile.js',
                     'app/scripts/{,*/}*.js'
                 ]
+            }
+        },
+        useminPrepare: {
+            html: 'app/views/index.html',
+            options: {
+                dest: 'dist'
+            }
+        },
+        concat: {
+            options:{
+                separator: ';'
             },
-            copy: {
-                dist: {
-                    cwd: 'app',
-                    src: ['**','!styles/**/*.css','!scripts/**/*.js'],
-                    dest: 'dist',
-                    expand: true
-                },
-                fonts: {
-                    files: [
-                        {
-                            //for boostrap fonts
-                            //tendriamos que cambiarlo por materialize
-                            expand:true,
-                            dot: true,
-                            cwd: 'bower_components/boostrap/dist',
-                            src: ['fonts/*.*'],
-                            dest: 'dist'
-                        },  {
-                            //aca iria lo de font-awesome pero no lo usamos...
-                        }
+            //dist configuration is provided bu useminPrepare
+            dist: {}
+        },
+        uglify: {
+            dist: {}
+        },
+        cssmin: {
+            dist: {}
+        },
+        filerev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'md5',
+                length: 20
+            },
+            release: {
+                files: [{
+                    src: [
+                        'dist/scripts/*.js',
+                        'dist/styles/*.css',
                     ]
-                }
+                }]
+            }
+        },
+        usemin: {
+            html: ['dist/views/*.html'],
+            css: ['dist/styles/*.css'],
+            options: {
+                assetsDirs: ['dist', 'dist/styles']
+            }
+        },
+        copy: {
+            dist: {
+                cwd: 'app',
+                src: ['**','!styles/**/*.css','!scripts/**/*.js'],
+                dest: 'dist',
+                expand: true
             },
-
-            clean: {
-                build: {
-                    src: ['dist/']
+            fonts: {
+                files: [
+                    {
+                        //for boostrap fonts
+                        //tendriamos que cambiarlo por materialize
+                        expand:true,
+                        dot: true,
+                        cwd: 'bower_components/boostrap/dist',
+                        src: ['fonts/*.*'],
+                        dest: 'dist'
+                    },  {
+                        //aca iria lo de font-awesome pero no lo usamos...
+                    }
+                ]
+            }
+        },
+        watch: {
+            copy: {
+                files: [ 'app/**', '!app/**/*.css', '!app/**/*.js'],
+                tasks: [ 'build' ]
+            },
+            scripts: {
+                files: ['app/scripts/app.js'],
+                tasks:[ 'build']
+            },
+            styles: {
+                files: ['app/styles/app.css'],
+                tasks:['build']
+            },
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: [
+                    'app/{,*/}*.html',
+                    '.tmp/styles/{,*/}*.css',
+                    'app/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+                ]
+            }
+        },
+        connect: {
+            options: {
+                port: 9000,
+                // Change this to '0.0.0.0' to access the server from outside.
+                hostname: 'localhost',
+                livereload: 35729
+            },
+            dist: {
+                options: {
+                    open: true,
+                    base:{
+                        path: 'dist',
+                        options: {
+                            index: 'index.html',
+                            maxAge: 300000
+                        }
+                    }
                 }
+            }
+        },
+        clean: {
+            build: {
+                src: ['dist/']
             }
         }
     });
@@ -56,8 +142,16 @@ module.exports = function(grunt){
     grunt.registerTask('build', [
         'clean',
         'jshint',
-        'copy'
+        'useminPrepare',
+        'concat',
+        'cssmin',
+        'uglify',
+        'copy',
+        'filerev',
+        'usemin'
     ]);
+
+    grunt.registerTask('serve',['build','connect:dist','watch']);
 
     grunt.registerTask('default',[
         'build'
