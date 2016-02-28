cb4c522fbda9067957d048e685588619b9be0f14
RicardoFazzi <ricardofazzi007@gmail.com>
2016-02-27 22:12:02 -0300

    agrego paginas nuevas y un nuevo controlador. Termina week 2 del curso de AngularJS

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,6 +5,11 @@ angular.module('AdminPro', [])
         $scope.tab = 1;
         $scope.filtText = "";
 
+        $scope.showDetails = false;
+        $scope.toggleDetails = function(){
+            $scope.showDetails = !$scope.showDetails;
+        };
+
         $scope.select = function(index){
             $scope.tab = index;
             if(index === 2){
@@ -64,4 +69,42 @@ angular.module('AdminPro', [])
             }
         ];
 
-    }]);
+    }])
+
+    .controller('ContactCtrl', ['$scope', function($scope){
+
+        $scope.feedback = {
+            mychannel:"",
+            firstName:"",
+            lastName: "",
+            agree:false,
+            email:""
+        };
+
+        $scope.channels = [{value:"tel", label:"Tel."},
+            {value:"Email", label:"Email"}];
+        $scope.invalidChannelSelection = false;
+
+    }])
+
+    .controller('FeedbackCtrl', ['$scope', function($scope){
+
+        $scope.sendFeedback = function(){
+            if ($scope.feedback.agree &&
+                ($scope.feedback.mychannel === "") && !$scope.feedback.mychannel) {
+                $scope.invalidChannelSelection = true;
+                console.log('incorrect');
+            }
+            else {
+                $scope.invalidChannelSelection = false;
+                $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+                    agree:false, email:"" };
+                $scope.feedback.mychannel="";
+
+                $scope.feedbackForm.$setPristine();
+                console.log($scope.feedback);
+            }
+
+        };
+    }])
+;
