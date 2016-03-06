96991e78ff799b710de47234ddeeaa9d47fd5a4d
RicardoFazzi <ricardofazzi007@gmail.com>
2016-03-06 20:44:52 -0300

    unit-test: Agrego unit test que usan el framework 'jasmine' y se corren con 'karma start karma.conf.js'

---
+++ test/unit/controllers/mainController.js
@@ -0,0 +1,76 @@
+describe('Controller: MainController', function () {
+
+    // load the controller's module
+    beforeEach(module('AdminPro'));
+
+    var MainController, scope, $httpBackend;
+
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function ($controller, _$httpBackend_,  $rootScope, mainService) {
+
+        // place here mocked dependencies
+        $httpBackend = _$httpBackend_;
+
+        $httpBackend.expectGET("http://localhost:3000/dishes").respond([
+            {
+                "id": 0,
+                "name": "Uthapizza",
+                "image": "images/uthapizza.png",
+                "category": "mains",
+                "label": "Hot",
+                "price": "4.99",
+                "description": "A",
+                "comments":[{}]
+            },
+            {
+                "id": 1,
+                "name": "Zucchipakoda",
+                "image": "images/zucchipakoda.png",
+                "category": "mains",
+                "label": "New",
+                "price": "4.99",
+                "description": "A",
+                "comments":[{}]
+            }
+        ]);
+
+        scope = $rootScope.$new();
+        MainController = $controller('MainController', {
+            $scope: scope, mainService: mainService
+        });
+        $httpBackend.flush();
+
+    }));
+
+    it('should have showDetails as false', function () {
+
+        expect(scope.showDetails).toBeFalsy();
+
+    });
+
+    it('should create "dishes" with 2 dishes fetched from xhr', function(){
+
+        expect(scope.showMenu).toBeTruthy();
+        expect(scope.dishes).toBeDefined();
+        expect(scope.dishes.length).toBe(2);
+
+    });
+
+    it('should have the correct data order in the dishes', function() {
+
+        expect(scope.dishes[0].name).toBe("Uthapizza");
+        expect(scope.dishes[1].label).toBe("New");
+
+    });
+
+    it('should change the tab selected based on tab clicked', function(){
+
+        expect(scope.tab).toEqual(1);
+
+        scope.select(3);
+
+        expect(scope.tab).toEqual(3);
+        expect(scope.filtText).toEqual('mains');
+
+    });
+});
